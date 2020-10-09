640f19b148fd159a6fd117df7315ab0cb670343a
Mengli Elmendorf <myuan@vai.net>
2020-10-09 17:01:40 -0400

    open tracking information from browser implementation

--- app/src/main/java/com/vormittag/mobile/tracker/MainActivity.kt
+++ app/src/main/java/com/vormittag/mobile/tracker/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.vormittag.mobile.tracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trackNoInput: EditText
@@ -27,17 +30,43 @@ class MainActivity : AppCompatActivity() {
             val number = trackNoInput.text.toString().trim()
             if (TrackingNumberValidation().isValidUPSTrackingNumber(number)){
                 type.text = "It is a ups tracking"
+                goToUPSTrackingInformation(number)
             }
             else if(TrackingNumberValidation().isValidUSPSTrackingNumber(number)) {
                 type.text = "It is a usps tracking"
+                goToUSPSTrackingInformation(number)
             }
             else if(TrackingNumberValidation().isValidFedexTrackingNumber(number)) {
                 type.text = "It is a fedex tracking"
+                goToFedexTrackingInformation(number)
             }
             else {
                 type.text = "Unknown tracking."
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        type.text = ""
+    }
+
+    private fun goToUPSTrackingInformation(number:String){
+        val url = "https://www.ups.com/track?loc=en_US&tracknum=$number"
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
+
+    private fun goToFedexTrackingInformation(number:String){
+        val url = "https://www.fedex.com/apps/fedextrack/?action=track&trackingnumber=%@&cntry_code=us$number"
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
+
+    private fun goToUSPSTrackingInformation(number: String){
+        val url = "https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=$number"
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
     }
 
 
